2dbd37e67c16c02b2d2be0f6be68fa347e1c457f
Adriaan Bakker <adriaanjbakker@gmail.com>
2024-12-12 21:27:13 +1300

    Updated to 5.4

--- Source/Ue5TestingEditor.Target.cs
+++ Source/Ue5TestingEditor.Target.cs
@@ -8,7 +8,7 @@ public class Ue5TestingEditorTarget : TargetRules
 	public Ue5TestingEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "Ue5Testing" } );
 	}
